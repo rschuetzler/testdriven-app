6b5c76934c2d3fcd2ed2d54b6d31769ba754c335
Ryan Schuetzler <ryan@schuetzler.net>
2018-04-10 13:54:11 -0500

    Give it a database!

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
 import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # start the app
 app = Flask(__name__)
@@ -8,6 +9,21 @@ app = Flask(__name__)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+# Set up models
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 
 
 @app.route('/users/ping', methods=['GET'])
