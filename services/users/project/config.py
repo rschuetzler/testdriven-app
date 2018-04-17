59ea1b6f3e094b8243d92f2a48befa5c5a17e401
Ryan Schuetzler <ryan@schuetzler.net>
2018-04-16 20:05:06 -0500

    Finish setting up testing

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,11 +5,13 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
     '''Testing Configuration'''
