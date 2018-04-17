e3fc6ba08614268873ce80eb987977421e783ba1
Ryan Schuetzler <ryan@schuetzler.net>
2018-04-16 19:14:15 -0500

    Add a database

---
+++ services/users/project/tests/test_config.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+
+from flask import current_app
+from flask_testing import TestCase
+
+from project import app
+
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
+        self.assertFalse(current_app is None)
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL'))
