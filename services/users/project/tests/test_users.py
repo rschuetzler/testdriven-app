32326dcf6579dfda385b9ee267e2b62ecda4d3ea
Ryan Schuetzler <ryan@schuetzler.net>
2018-04-19 21:50:47 -0500

    start adding API tests

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -15,6 +15,22 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        """Ensure a new user can be added"""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'ryan',
+                    'email': 'ryan@example.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('ryan@example.com was added!', data['message'])
+            self.assertIn('success', data['success'])
+
 
 if __name__ == '__main__':
     unittest.main()
