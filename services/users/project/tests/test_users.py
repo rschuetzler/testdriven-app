2e2bdf247413b5462882b8665aa65854b3c62009
Ryan Schuetzler <ryan@schuetzler.net>
2018-04-20 17:11:48 -0500

    Do some error handling (WIP)

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -31,6 +31,37 @@ class TestUserService(BaseTestCase):
             self.assertIn('ryan@example.com was added!', data['message'])
             self.assertIn('success', data['status'])
 
+    def test_add_user_invalid_json(self):
+        """Ensure error is thrown if JSON is empty"""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'email': 'ryan@example.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_email(self):
+        """Ensure error is thrown if the email already exists"""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'ryan',
+                    'email': 'ryan@example.com'
+                }),
+                content_type='application/json',
+                )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('email already exists', data['message'])
+            self.assertIn('fail', data['status'])
+
 
 if __name__ == '__main__':
     unittest.main()
