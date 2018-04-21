38c3542e1e656993468a60a6b3e0930d7ee55831
Ryan Schuetzler <ryan@schuetzler.net>
2018-04-21 15:48:04 -0500

    Format fix

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -40,10 +40,7 @@ def add_user():
 @users_blueprint.route('/users/<user_id>', methods=['GET'])
 def get_single_user(user_id):
     """Get single user details"""
-    response_object = {
-        'status': 'fail',
-        'message': 'User does not exist'
-    }
+    response_object = {'status': 'fail', 'message': 'User does not exist'}
     try:
         user = User.query.filter_by(id=user_id).first()
         if not user:
@@ -64,6 +61,7 @@ def get_single_user(user_id):
     except exc.DataError:
         response_object['message'] = 'No user ID provided'
         return jsonify(response_object), 404
+
 
 @users_blueprint.route('/users', methods=['GET'])
 def get_all_users():
