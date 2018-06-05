9b49ef08d9301ea11f4e8bff79a0d06be1edf41c
Ryan Schuetzler <ryan@schuetzler.net>
2018-06-04 20:53:08 -0500

    Add a UI

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from project import db
 from project.api.models import User
 from sqlalchemy import exc
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
 
 @users_blueprint.route('/users/ping', methods=['GET'])
@@ -73,3 +73,7 @@ def get_all_users():
         }
     }
     return jsonify(response_object), 200
+
+@users_blueprint.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
