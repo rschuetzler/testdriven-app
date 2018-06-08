ee35f5f27b5393dfa834c9e5f20adc7f313cb26b
Ryan Schuetzler <ryan@schuetzler.net>
2018-06-08 15:33:38 -0500

    Make the basic React site work

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import UsersList from './components/UsersList';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: []
+        };
+    };
+
+    componentDidMount() {
+        this.getUsers();
+    };
+
+    getUsers() {
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+        .then((res) => { this.setState({ users: res.data.data.users }); })
+        .catch((err) => { console.log(err); })
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <br />
+                        <h1>All Users</h1>
+                        <hr /><br />
+                        <UsersList users={this.state.users} />
+                    </div>
+                </div>
+            </div>
+        )
+    };
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
 registerServiceWorker();
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
@@ -10,6 +11,8 @@ toolbar = DebugToolbarExtension()
 def create_app(script_info=None):
 
     app = Flask(__name__)
+
+    CORS(app)
 
     # set config
     app_settings = os.getenv("APP_SETTINGS")
