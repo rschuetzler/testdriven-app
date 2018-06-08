6e8002561a4d94de2fa8eca36e9becf1b9cae68b
Ryan Schuetzler <ryan@schuetzler.net>
2018-06-08 14:23:39 -0500

    Add flask toolbar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -10,14 +12,17 @@ def create_app(script_info=None):
     app = Flask(__name__)
 
     # set config
-    app_settings = os.getenv('APP_SETTINGS')
+    app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
+    # Set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     from project.api.users import users_blueprint
+
     app.register_blueprint(users_blueprint)
 
     # Shell context for flask CLI
-    app.shell_context_processor({'app': app, 'db': db})
+    app.shell_context_processor({"app": app, "db": db})
     return app
--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -19,6 +19,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertFalse(current_app is None)
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] ==
                         os.environ.get('DATABASE_URL'))
+        self.assertTrue(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestTestingConfig(TestCase):
@@ -32,6 +33,7 @@ class TestTestingConfig(TestCase):
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] ==
                         os.environ.get('DATABASE_TEST_URL'))
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestProductionConfig(TestCase):
@@ -42,6 +44,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 if __name__ == '__main__':
