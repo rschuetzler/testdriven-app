9c4faea94c94dd2220bd1ce486164d7c6c049cc1
Ryan Schuetzler <ryan@schuetzler.net>
2018-06-27 15:56:44 -0500

    Add some react bootstrapping

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
 
 class App extends Component {
     constructor() {
@@ -13,6 +14,7 @@ class App extends Component {
             users: [],
             username: '',
             email: '',
+            title: 'TestDriven.io'
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -50,27 +52,30 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <br />
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr /><br />
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        handleChange={this.handleChange}
-                                        addUser={this.addUser}
-                                    />
-                                    <br />
-                                    <UsersList users={this.state.users} />
-                                </div>
-                            )} />
-                            <Route exact path='/about' component={About} />
-                        </Switch>
+            <div>
+                <NavBar title={this.state.title} />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <br />
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr /><br />
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            handleChange={this.handleChange}
+                                            addUser={this.addUser}
+                                        />
+                                        <br />
+                                        <UsersList users={this.state.users} />
+                                    </div>
+                                )} />
+                                <Route exact path='/about' component={About} />
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
